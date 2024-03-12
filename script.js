b36d0be07459dbb72a7ac5e592f70b06527a33a3
e21krida <e21krida@student.his.se>
2024-03-12 11:45:55 +0100

    Importing of baseline code.
    Importing baseline code from previous project, establishing the foundation for further development.

---
+++ script.js
@@ -0,0 +1,28 @@
+import * as THREE from 'three';
+console.log("Script loaded");
+function generateCanvases() {
+  const canvasContainer = document.querySelector('.canvas-container');
+
+  for (let i = 1; i <= 12; i++) {
+    const canvasWrapper = document.createElement('div');
+    canvasWrapper.classList.add('canvas-wrapper');
+    const canvas = document.createElement('canvas');
+    canvas.id = `canvas${i}`;
+    canvasWrapper.appendChild(canvas);
+    const text = document.createElement('div');
+    text.classList.add('canvas-text');
+    text.textContent = `Canvas ${i}`;
+    canvasWrapper.appendChild(text);
+    canvasContainer.appendChild(canvasWrapper);
+
+    generateThree(canvas.id);
+  }
+}
+
+function generateThree(canvas) {
+  console.log(canvas)
+}
+
+window.onload = function () {
+  generateCanvases();
+};
