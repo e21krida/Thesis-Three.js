4dde01c5ff71f63341a99e23914195fb06060b3b
e21krida <e21krida@student.his.se>
2024-03-14 09:58:46 +0100

    Installed Stats.js
    Installed Stats.js and implemented a stats-canvas for each primary canvas to read and analyze frames per second.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import Stats from 'stats.js';
 
 console.log("Script loaded");
 function generateCanvases() {
@@ -18,12 +19,18 @@ function generateCanvases() {
     canvasWrapper.appendChild(text);
     canvasContainer.appendChild(canvasWrapper);
 
+    const statsContainer = document.createElement('div');
+    statsContainer.classList.add('stats-container');
+    canvasWrapper.appendChild(statsContainer);
     console.log(`Generating canvas: canvas${i}`);
-    generateThree(`canvas${i}`);
+    generateThree(`canvas${i}`, statsContainer);
   }
 }
 
-function generateThree(canvasId) {
+function generateThree(canvasId, statsContainer) {
+  var stats = new Stats();
+  stats.showPanel(0);
+  statsContainer.appendChild(stats.dom);
   const canvas = document.getElementById(canvasId);
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000);
@@ -36,10 +43,12 @@ function generateThree(canvasId) {
   scene.add(cube);
 
   function animate() {
+    stats.begin();
     requestAnimationFrame(animate);
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
     renderer.render(scene, camera);
+    stats.end();
   }
 
   animate();
